e374c6577f3ab89540770e70267d125817a36f12
Deborah Reyes <deborahreyes@Deborahs-MBP.localdomain>
2018-10-09 15:28:17 -0700

    allowed for username parameter in user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   respond_to :json
-  include ActionController::MimeResponds 
+  include ActionController::MimeResponds
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :username])
+  end
 end
